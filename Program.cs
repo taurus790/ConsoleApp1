90f22873a467d8ddaa32ab62612240b0ff8ef081
Yadigar Muradov <yadigar-m@hotmail.com>
2019-09-28 09:08:38 +0200

    Video01

--- Program.cs
+++ Program.cs
@@ -8,8 +8,21 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            byte myByte = 255;                       // 8 bits
+            char myChar = 'a';                       // 16 bits
+            short myShort = 65;                      // 16 bits
+            int myInt = 2147483647;                  // 32 bits
+            uint myUInt = 4294967295;                // 32 bits
+            float myFloat = 1.23456f;                // 32 bits
+            double myDouble = 1.23456;               // 64 bits
+            decimal myDecimal = (decimal)1.23456;    // 128 bits 
+            string myString = "Hello World!";        // Text
+            bool myBool = true;                      // true or false 
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
