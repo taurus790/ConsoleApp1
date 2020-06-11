b4ba0d35176d500d574cbc5b9e386bbbc952a543
Yadigar Muradov <yadigar-m@hotmail.com>
2020-06-11 22:40:01 +0200

    commit 200611 2239

--- Program.cs
+++ Program.cs
@@ -21,21 +21,21 @@ namespace ConsoleApp1
     {
         // Data types 
 
-        byte myByte = 255;                       // 8 bits
-        char myChar = 'a';                       // 16 bits
+        byte myByte = 255;                       // 8 bits  
+        char myChar = 'a';                       // 16 bits  
         short myShort = 65;                      // 16 bits
         int myInt = 2147483647;                  // 32 bits
-        uint myUInt = 4294967295;                // 32 bits
+        uint myUInt = 4294967295;                // 32 bits  
         float myFloat = 1.23456f;                // 32 bits
         double myDouble = 1.23456;               // 64 bits
-        decimal myDecimal = (decimal)1.23456;    // 128 bits 
-        string myString = "Hello World!";        // Text
-        bool myBool = true;                      // true or false 
+        decimal myDecimal = (decimal)1.23456;    // 128 bits   
+        string myString = "Hello World!";        // Text 
+        bool myBool = true;                      // true or false  
     }
 
     class Video2
     {
-        // 1. Comments 
+        // 1. Comments  
         /* 
          * Multi 
          * line 
@@ -81,7 +81,7 @@ namespace ConsoleApp1
                 else
                 {
                     // 7. Calculations
-                    userNumber *= 2; // userNumber = userNumber * 2;
+                    userNumber *= 1; // userNumber = userNumber * 2;
 
                     // 8. Constructing strings from variables 
                     Console.WriteLine($"Your new number is {userNumber}."); //Console.WriteLine("Your new number is " + userNumber + ".");
